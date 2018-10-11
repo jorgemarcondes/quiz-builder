a092e7eed10c0dc1164c5e5d0e454f16625f3bd2
jorgealberto <jorge.alberto@starlinetecnologia.com.br>
2018-10-11 19:25:24 -0300

    - Adiciona templates de quizitem e simpletextitem

---
+++ src/components/QuizItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <pf-quiz-section>
+    <div v-if=""></div>
+  </pf-quiz-section>
+</template>
+
+<script>
+  import PfQuizSection from './PfQuizSection'
+
+  export default {
+    name: "QuizItem",
+    components: { PfQuizSection }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/QuizSimpleTextItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <pf-quiz-section>
+
+  </pf-quiz-section>
+</template>
+
+<script>
+
+  export default {
+    name: 'QuizSimpleTextItem',
+  }
+</script>
+
+<style scoped>
+
+</style>
